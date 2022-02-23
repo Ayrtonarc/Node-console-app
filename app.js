a6019e338cac0129ef80abb27d6cee8f63860fe1
General Krypton <ayrton.arc@gmail.com>
2022-02-22 16:23:23 -0800

    collecting data from terminal

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+
 console.clear();
 
 const main = async() => {
     console.log('Hola mundo');
+
+    mostrarMenu();
+
+    pausa();
 }
 
 main();
