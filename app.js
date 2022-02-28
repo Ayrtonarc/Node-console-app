020c074b2cde2dad159b48febd7a95766d3d4e40
Ayrton <ayrton.arc@gmail.com>
2022-02-28 11:36:50 -0800

    interactive menu options

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('colors');
-const { inquirerMenu } = require('./helpers/inquirer');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
+
 
 
 
@@ -16,7 +17,7 @@ const main = async() => {
       opt = await inquirerMenu();
         console.log({ opt });
 
-      
+      await pausa();
 
     }while(opt !== '0');
     
