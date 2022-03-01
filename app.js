5347fb664ff373819da59f07b3f8152396b7dc56
Ayrton <ayrton.arc@gmail.com>
2022-02-28 21:45:44 -0800

    logic for the task to do

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 require('colors');
 const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 
 
-
-
-
-
-
-console.clear();
 
 const main = async() => {
     console.log('Hola mundo');
 
     let opt = '';
+    
     do {
       opt = await inquirerMenu();
-        console.log({ opt });
+      console.log({ opt });
+
 
       await pausa();
 
