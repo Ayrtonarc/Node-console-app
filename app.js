03805ac197227c2ea9414b564ffe1cee099a9675
Ayrton <ayrton.arc@gmail.com>
2022-03-01 14:34:34 -0800

    Create list and tasks

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 require('colors');
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
 const Tareas = require('./models/tareas');
 
 
 
 const main = async() => {
-    console.log('Hola mundo');
+    
 
     let opt = '';
-    
+    const tareas = new Tareas();
     do {
       opt = await inquirerMenu();
-      console.log({ opt });
+      
+      switch (opt) {
+        case '1':
+          const desc = await leerInput('Descripcion: ');
+          tareas.crearTarea( desc );
+        break;
 
+        case '2':
+          console.log(tareas._listado);
+          break;
+      }
 
       await pausa();
 
