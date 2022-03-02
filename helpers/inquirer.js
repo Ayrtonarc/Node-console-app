6232b1a091798598aa3698db203f9c11b5a7e49f
Ayrton <ayrton.arc@gmail.com>
2022-03-02 13:01:29 -0800

    transforming object in array to show tha task

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -8,31 +8,31 @@ const preguntas = [{
     choices: [
         {
             value: '1',
-            name: '1. Crear tarea'
+            name: `${'1.'} Crear tarea`
         },
         {
             value: '2',
-            name: '2. Listar tareas'
+            name: `${'2.'} Listar tarea`
         },
         {
             value: '3',
-            name: '3. Listar tareas completadas'
+            name: `${'3.'} Listar tareas completadas`
         },
         {
             value: '4',
-            name: '4. Listar tareas pendientes'
+            name: `${'4.'} Listar tareas pendientes`
         },
         {
             value: '5',
-            name: '5. Completar tareas'
+            name: `${'5.'} Completar tarea`
         },
         {
             value: '6',
-            name: '6. Borrar tarea'
+            name: `${'6.'} Borrar  tarea`
         },
         {
             value: '0',
-            name: '0. Salir'
+            name: `${'0.'} Salir \n`
         }
              ]
     }
